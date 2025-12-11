4834cec8306cc7cf8095a61723dd539fb7ac9e36
ChangLae <chochanglae1223@gmail.com>
2025-12-11 15:59:59 +0900

    20251211 보드 마이페이지(3)

--- boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
+++ boardProject/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -1,16 +1,21 @@
 package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
-	
-	@RequestMapping("/")	// / 요청 매핑
-	public String mainPage() {
-
-		// 접두사/접미사 제외
-		// classpath : src/main/resources/templates/**common/main**.html
-		return "common/main";	// forward 한다.
-	}
+    
+    @RequestMapping("/")	// / 요청 매핑
+    public String mainPage() {
+        return "common/main";	// forward
+    }
+    
+    @GetMapping("loginError")
+    public String loginError(RedirectAttributes ra) {
+        ra.addFlashAttribute("message", "로그인 후 이용해주세요~");
+        return "redirect:/";
+    }
 }
